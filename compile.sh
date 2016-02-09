0bde5d43962476508b4fbbaf86dfe4af311a462f
Jesse Waite <Jesse_Waite@hotmail.com>
2016-02-08 16:12:41 -0800

    updated build script

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
-echo "compiling seqcomp..."
-g++ -o seqcomp main.cpp Source.cpp -std=c++0x -o2
+echo "compiling seqcomp, -std=c++0x and -O2 optimizations..."
+g++ -o seqcomp main.cpp Source.cpp -std=c++0x -O2
